88330778f2cc24b886e65a6b2fc7bf8f9cbcf437
betta7635 <betta7635@yahoo.com>
2022-06-15 08:26:43 -0500

    wishlist
    create wishlist, edit-wishlist, new-wishlist, wishlist-details, wishlist-index

--- src/app/wishlist/new-wishlist/new-wishlist.component.ts
+++ src/app/wishlist/new-wishlist/new-wishlist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Wishlist } from '../wishlist.model';
+import { WishlistService } from '../wishlist.service';
 
 @Component({
   selector: 'app-new-wishlist',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-wishlist.component.css']
 })
 export class NewWishlistComponent implements OnInit {
+  myWishlists: Wishlist[] = [];
+  selectedWishlist: Wishlist;
+  wishlist: Wishlist;
+  idx: number;
 
-  constructor() { }
+  constructor(private wishlistService: WishlistService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.idx = +params['id'];
+      this.wishlist = this.wishlistService.getWishlist(this.idx);
+    });
+    this.myWishlists = this.wishlistService.getWishlists();
+    this.wishlistService.wishlistIndexChanged.subscribe((wishlists: Wishlist[]) => {
+      this.myWishlists = wishlists;
+    });
+    this.wishlistService.wishlistSelected.subscribe((wishlist: Wishlist) => {
+      this.selectedWishlist = wishlist;
+    });
   }
 
+  onSaveWishlist(wishlist) {
+    // onsubmit(ngsubmit)??
+    // redirect to new wishlist id details
+
+  }
+
+  onCancel() {
+
+    // redirect to wishlist index
+
+  }
 }
--- src/app/wishlist/wishlist-details/wishlist-details.component.ts
+++ src/app/wishlist/wishlist-details/wishlist-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Info } from 'src/app/shared/info-form/info.model';
+import { Wishlist } from '../wishlist.model';
+import { WishlistService } from '../wishlist.service';
 
 @Component({
   selector: 'app-wishlist-details',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wishlist-details.component.css']
 })
 export class WishlistDetailsComponent implements OnInit {
+  myWishlists: Wishlist[] = [];
+  selectedWishlist: Wishlist;
+  wishlist: Wishlist;
+  idx: number;
+  index: number;
 
-  constructor() { }
+  constructor(private wishlistservice: WishlistService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.idx = +params['id'];
+      this.wishlist = this.wishlistservice.getWishlist(this.idx);
+    });
+    this.myWishlists = this.wishlistservice.getWishlists();
+    this.wishlistservice.wishlistSelected.subscribe((wishlist: Wishlist) => {
+      this.selectedWishlist = wishlist;
+    });
+    this.wishlistservice.wishlistIndexChanged.subscribe((wishlists: Wishlist[]) => {
+      this.myWishlists = wishlists;
+    });
   }
 
+  onEditWishlist(wishlist) {
+
+    // redirect to current id edit form
+
+  }
+
+  onCancel() {
+
+    // redirect to wishlist index
+
+  }
+
+  onDeleteWishlist(index) {
+
+    // redirect to wishlist index
+
+  }
 }
